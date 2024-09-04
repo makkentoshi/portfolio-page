15e17545bcb94f4e43f6952fe7edf6e066d1e324
makkentoshi <pochta.zdraste@mail.ru>
2024-09-05 01:22:46 +0500

    chore: Update hero ring shadow and add HeroOrbit component

---
+++ components/HeroOrbit.tsx
@@ -0,0 +1,33 @@
+import StarIcon from "@/assets/icons/star.svg";
+import Image from "next/image";
+import { PropsWithChildren } from "react";
+
+const HeroOrbit = ({
+  children,
+  size,
+  rotation,
+}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 size-[800px] ">
+      <div
+        className=""
+        style={{
+          transform: `rotate(${rotation}deg)`,
+          height: `${size}px`,
+          width: `${size}px`,
+        }}
+      >
+        <div
+          className="inline-flex "
+          style={{
+            transform: `rotate(${rotation * -1}deg)`,
+          }}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroOrbit;
--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -3,16 +3,25 @@ import memojiImage from "@/assets/images/brown-hair-guy-with-laptop.png";
 import ArrowSign from "@/assets/icons/arrow-down.svg";
 import WaveEmoji from "@/assets/icons/wave.png";
 import grainImage from "@/assets/images/grain.jpg";
+import StarIcon from "@/assets/icons/star.svg";
 import Image from "next/image";
+import HeroOrbit from "@/components/HeroOrbit";
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
       <div
         className="absolute inset-0 -z-30 opacity-5"
         style={{ backgroundImage: `url(${grainImage.src}` }}
       ></div>
       <div className="size-[620px] hero-ring"></div>
+      <div className="size-[820px] hero-ring"></div>
+      <div className="size-[1020px] hero-ring"></div>
+      <div className="size-[1220px] hero-ring"></div>
+      <HeroOrbit size={800} rotation={-72}>
+        <StarIcon className="size-28 text-emerald-300"></StarIcon>
+      </HeroOrbit>
+
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
@@ -37,12 +46,13 @@ export const HeroSection = () => {
             web applications. Let's discuss your next project
           </p>
         </div>
-        <div className="flex flex-col md:flex-row justify-center items-center mt-8">
-          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8  ">
+          
+          <button className="inline-flex items-center gap-2 border border-white/15 px-5 h-12 rounded-xl ">
             <span className="font-semibold">Explore My Work</span>
-            <Image src={ArrowSign} alt="arrow sign"></Image>
+            <ArrowSign className="w-4 h-4"></ArrowSign>
           </button>
-          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 sm:px-5 px-7 rounded-xl">
             <span>
               <Image src={WaveEmoji} alt="wave" width={20} height={20}></Image>
             </span>
