f2f502330e57116dfbc9e6540ab4627bb07b5155
makkentoshi <pochta.zdraste@mail.ru>
2024-09-10 00:35:09 +0500

    refactor: Update TestimonialsSection styling and add responsive layout for testimonials

--- sections/Testimonials.tsx
+++ sections/Testimonials.tsx
@@ -50,16 +50,30 @@ export const TestimonialsSection = () => {
           title="What Clients Say about Me"
           description="Don't just take my word for it. See what my clients have to say about my work."
         ></SectionHeader>
-        /
-        <div>
-          {testimonials.map((testimonial) => (
-            <Card key={testimonial.name}>
-              <Image src={testimonial.avatar} alt={testimonial.name}></Image>
-              <div>{testimonial.name}</div>
-              <div>{testimonial.position}</div>
-              <p>{testimonial.text}</p>
-            </Card>
-          ))}
+
+        <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%)]">
+          <div className="flex gap-8 flex-none">
+            {testimonials.map((testimonial) => (
+              <Card key={testimonial.name} className="max-w-xs md:p- md:max-w-md">
+                <div className="flex gap-4 items-center">
+                  <div className="size-14 bg-gray-700 inline-flex rounded-full items-center justify-center flex-shrink-0">
+                    <Image
+                      src={testimonial.avatar}
+                      alt={testimonial.name}
+                      className="max-h-full"
+                    ></Image>
+                  </div>
+                  <div>
+                    <div className="font-semibold">{testimonial.name}</div>
+                    <div className="text-sm text-white/40">
+                      {testimonial.position}
+                    </div>
+                  </div>
+                </div>
+                <p className="mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
     </div>
